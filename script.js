a4ad839a245453edfa950dcc38adfb5ec8e74a93
DennisHvA <dennis.de.graaf2@hva.nl>
2023-06-07 13:05:27 +0200

    casper

--- script.js
+++ script.js
@@ -1,34 +1,44 @@
-const apiUrl = 'https://api.dataplatform.knmi.nl/open-data/v1/datasets/ROYK/radius';
-const apiKey = 'eyJvcmciOiI1ZTU1NGUxOTI3NGE5NjAwMDEyYTNlYjEiLCJpZCI6IjI4ZWZlOTZkNDk2ZjQ3ZmE5YjMzNWY5NDU3NWQyMzViIiwiaCI6Im11cm11cjEyOCJ9'; // Vervang dit met je eigen KNMI API-sleutel
+let currentStep = 1;
+const circles = document.getElementsByClassName('circle');
+const steps = document.getElementsByClassName('step');
 
-// Functie om het fetch-verzoek uit te voeren
-async function fetchNeerslagData() {
-  const start = '2023-01-01T00:00:00Z'; // Vervang met de gewenste startdatum
-  const end = '2023-01-31T23:59:59Z'; // Vervang met de gewenste einddatum
-  const lat = 52.0; // Vervang met de gewenste breedtegraad
-  const lon = 5.0; // Vervang met de gewenste lengtegraad
-  const radius = 10; // Vervang met de gewenste straal in kilometers
-
-  const url = `${apiUrl}?start=${start}&end=${end}&lat=${lat}&lon=${lon}&radius=${radius}&format=json`;
-
-  try {
-    const response = await fetch(url, {
-      headers: {
-        Authorization: apiKey,
-        'Access-Control-Allow-Origin': '*'
-      }
-    });
-
-    if (response.ok) {
-      const data = await response.json();
-      console.log(data); // Hier kun je de ontvangen gegevens verder verwerken
+function updateProgressIndicator() {
+  for (var i = 0; i < circles.length; i++) {
+    if (i === currentStep - 1) {
+      circles[i].style.backgroundColor = 'var(--color-green)';
     } else {
-      console.log('Er is een fout opgetreden bij het ophalen van de gegevens.');
+      circles[i].style.backgroundColor = 'transparent';
     }
-  } catch (error) {
-    console.log('Er is een fout opgetreden bij het maken van het verzoek:', error);
   }
 }
 
-// Roep de functie aan om de neerslaggegevens op te halen
-fetchNeerslagData();
+updateProgressIndicator()
+
+function nextStep() {
+  if (currentStep < steps.length && document.getElementById('step-' + currentStep).checkValidity()) {
+    steps[currentStep - 1].classList.remove('active');
+    currentStep++;
+    steps[currentStep - 1].classList.add('active');
+    updateProgressIndicator();
+  }
+}
+
+function previousStep() {
+  if (currentStep > 1) {
+    steps[currentStep - 1].classList.remove('active');
+    currentStep--;
+    steps[currentStep - 1].classList.add('active');
+    updateProgressIndicator();
+  }
+}
+
+document.getElementById('step-0').querySelector('button').addEventListener('click', nextStep);
+document.getElementById('step-1').querySelector('button').addEventListener('click', nextStep);
+document.getElementById('step-2').querySelector('button:last-of-type').addEventListener('click', nextStep);
+document.getElementById('step-2').querySelector('button:first-of-type').addEventListener('click', previousStep);
+document.getElementById('step-3').querySelector('button:last-of-type').addEventListener('click', nextStep);
+document.getElementById('step-3').querySelector('button:first-of-type').addEventListener('click', previousStep);
+document.getElementById('step-4').querySelector('button:last-of-type').addEventListener('click', nextStep);
+document.getElementById('step-4').querySelector('button:first-of-type').addEventListener('click', previousStep);
+document.getElementById('step-5').querySelector('button:last-of-type').addEventListener('click', nextStep);
+document.getElementById('step-5').querySelector('button:first-of-type').addEventListener('click', previousStep);
