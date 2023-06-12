6c3141e16f690eb6c3d4203039baf3554e9c8c97
Nigel Fijnheer <34651169+Neinno@users.noreply.github.com>
2023-06-12 13:49:56 +0200

    update to homepage
    edit nav + main

--- src/routes/home/+page.svelte
+++ src/routes/home/+page.svelte
@@ -1,55 +1,45 @@
 <main class="landing">
     <header>
-            <section>
-                <h1>bRAIN</h1>
-    
-                <button class="dropdown-btn"><i class="fa-solid fa-bars"></i></button>
-            </section>
-            
-
-            <nav>
-                <ul>
-                    <li>
-                        <a href="#">Profiel</a>
-                    </li>
-                    <li>
-                        <a href="#">Instellingen</a>
-                    </li>
-                    <li>
-                        <a href="#">Informatie</a>
-                    </li>
-                </ul>
-            </nav>
-        </header>
+        <section>
+            <h1>bRAIN</h1>
+        </section>   
+    </header>
 
    <section class="info">
-
-        <h2>Spatwater's Regenton</h2>
-
         <section class="tiles">
             <div class="full-date">
-                <p>Verwachte vol datum</p>
+                <p class="info-text">Verwachte vol datum</p>
                 <p class="data-info">15 Juli</p>
             </div>
             <div class="rain-date">
-                <p>Volgende regenval</p>
+                <p class="info-text">Volgende regenval</p>
                 <p class="data-info">28 Juni</p>
             </div>
         </section>
 
-        <section class="tiles">
-            <div class="barrel">
-                <p>100/250</p>
-            </div>
-            
-            <div class="total-rain">
-                <p>Totaal water opgevangen</p>
-                <p class="data-info">568L</p>
-            </div>
-        </section>
-            
-        
+
+        <h2>Spatwater's Regenton</h2>
    </section>
 
-    <button class="leeg">Leeg</button>
+   <section class="barrel">
+    
+   </section>
+
+
+   <button class="leeg">Leeg</button>
+
+    <nav>
+        <ul>
+            <li>
+                <a href="#">Home</a>
+            </li>
+            <li>
+                <a href="#">Totaal opgevangen</a>
+            </li>
+            <li>
+                <a href="#">Profiel</a>
+            </li>
+        </ul>
+    </nav>
+
 </main>
