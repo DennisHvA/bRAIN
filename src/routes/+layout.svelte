b2e3cf95787424f42ac6f945511b752dee18f228
DennisHvA <dennis.de.graaf2@hva.nl>
2023-06-21 13:24:26 +0200

    installable

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
     import '../app.css'
+    // if ('serviceWorker' in navigator) {
+    //     window.addEventListener('load', () => {
+    //         navigator.serviceWorker.register('/service-worker.js')
+    //         .then(registration => {
+    //             console.log('Service Worker registered with scope:', registration.scope);
+    //         })
+    //         .catch(error => {
+    //             console.error('Service Worker registration failed:', error);
+    //         });
+    //     });
+    // }
+
 </script>
 
 <slot />
---
+++ src/service-worker.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', event => {
+    console.log('installing');
+});
+  
+self.addEventListener('fetch', event => {
+
+});
+  
