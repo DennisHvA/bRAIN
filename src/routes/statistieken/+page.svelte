5710bbbb4140f762eb32e21baa123193a137b58f
DennisHvA <dennis.de.graaf2@hva.nl>
2023-06-22 14:20:40 +0200

    hallo

--- src/routes/statistieken/+page.svelte
+++ src/routes/statistieken/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-    import Nav from '../../$lib/components/Nav.svelte'
-    import Header from '../../$lib/components/Header.svelte'
+    import Nav from '$lib/components/Nav.svelte'
+    import Header from '$lib/components/Header.svelte'
 </script>
 
 <Header />
