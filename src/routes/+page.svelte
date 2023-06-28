92ad3202d14d8ee2759947f83d232f95209e7ca0
Nigel Fijnheer <34651169+Neinno@users.noreply.github.com>
2023-06-28 14:15:59 +0200

    Landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,12 @@
     <h2>We helpen met het opvangen van regenwater!</h2>
     <p>Voordat we beginnen hebben we een aantal gegevens nodig</p>
     <a href="/onboarding">Start</a>
+    <section>
+        <img src="images/landing1.png" alt="berg">
+        <img src="images/landing2.png" alt="berg">
+        <img src="images/landing3.png" alt="berg">
+        <img src="images/landing4.png" alt="berg">
+    </section>
 </main>
 
 <style>
@@ -11,8 +17,12 @@
         flex-direction: column;
         gap: 1em;
         height: 100vh;
-        justify-content: center;
         align-items: center;
+
+    }
+
+    h1 {
+        padding-top:2em;
     }
 
     h2 {
@@ -27,5 +37,20 @@
 
     a {
         width: fit-content;
+    }
+
+    section {
+        z-index: -1;
+        position: absolute;
+        width: 100%;
+        height: 100%;
+    }
+
+    section img {
+        position: absolute;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        pointer-events: none;
     }
 </style>
